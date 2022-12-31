21b41a1d32ad8d7df26b217c8db7eeb7ec431fa4
lvke <lvke>
2022-12-31 09:48:21 +0800

    initial setting in project

--- Enternity/src/main.cpp
+++ Enternity/src/main.cpp
@@ -1,7 +1,49 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main()
 {
-	std::cout << "hello, enternity" << std::endl;
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	//gladLoadGL();
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		//std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+
+	glfwTerminate();
 	return 0;
 }
