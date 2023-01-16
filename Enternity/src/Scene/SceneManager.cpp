71aad509b0e98c28e29e3d6cfb54dcf9d2768417
ke.lv <lvke>
2023-01-16 16:25:21 +0800

    after draw do unbind

--- Enternity/src/Scene/SceneManager.cpp
+++ Enternity/src/Scene/SceneManager.cpp
@@ -121,6 +121,20 @@ void SceneManager::Tick(float deltaTime)
 				meshComponent.m_Indexbuffer->Bind();
 				CHECK_GL_CALL(glDrawElements(GL_TRIANGLES, meshComponent.m_Indexbuffer->GetCount(), GL_UNSIGNED_INT, (void*)0));
 			}
+			//unbind
+			if (meshComponent.m_VertexArray)
+				meshComponent.m_VertexArray->UnBind();
+
+			if (materialComponent.m_Shader)
+			{
+				materialComponent.m_Shader->UnBind();
+			}
+			if (materialComponent.m_Texture)
+				materialComponent.m_Texture->UnBind();
+			if (meshComponent.m_Indexbuffer)
+			{
+				meshComponent.m_Indexbuffer->UnBind();
+			}
 		}
 	}
 }
