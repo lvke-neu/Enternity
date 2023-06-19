0d8a26aee107c9d69ab182a187012246f8235d58
ke.lv <lvke>
2023-06-19 20:24:34 +0800

    fix GraphicsSystem provider nullptr bug

--- Enternity/src/Graphics/GraphicsSystem.cpp
+++ Enternity/src/Graphics/GraphicsSystem.cpp
@@ -1,18 +1,22 @@
 #include "GraphicsSystem.h"
 #include "RHI/Texture/TextureProvider.h"
 #include "RHI/Renderer/RendererProvider.h"
+#include "RHI/Mesh/MeshProvider.h"
 
 namespace Enternity
 {
 	GraphicsSystem::GraphicsSystem()
 	{
 		m_textureProvider = new TextureProvider;
+		m_rendererProvider = new RendererProvider;
+		m_meshProvider = new MeshProvider;
 	}
 
 	GraphicsSystem::~GraphicsSystem()
 	{
 		SAFE_DELETE_SET_NULL(m_textureProvider);
 		SAFE_DELETE_SET_NULL(m_rendererProvider)
+		SAFE_DELETE_SET_NULL(m_meshProvider)
 	}
 }
 
