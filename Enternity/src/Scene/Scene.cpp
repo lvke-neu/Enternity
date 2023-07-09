da024cabf1d35aa988b82154ec65bab8e20c8005
lvke <lvke>
2023-07-09 21:40:58 +0800

    nothing

--- Enternity/src/Scene/Scene.cpp
+++ Enternity/src/Scene/Scene.cpp
@@ -28,7 +28,7 @@ namespace Enternity
 		auto& comp = m_sceneCamera.addComponent<TransformComponent>();
 		comp.translation = glm::vec3(0.28, 8.41, 19.70);
 		comp.rotation = glm::vec3(-6.20, -11.00, 0);
-		m_sceneCamera.addComponent<CameraComponent>();
+		m_sceneCamera.addComponent<CameraComponent>().moveSpeed = 30;
 		m_cameraController = new CameraController(&m_sceneCamera);
 
 		//postprocess
@@ -119,7 +119,8 @@ namespace Enternity
 		auto entity4 = createEntity();
 		entity4.getComponent<NameComponent>().name = "madara_uchiha";
 		auto& tfc4 = entity4.addComponent<TransformComponent>();
-		tfc4.translation = glm::vec3(18.45, 10.00, 0);
+		tfc4.translation = glm::vec3(22.44, 0, -4.13);
+		tfc4.scale = glm::vec3(4.00, 4.00, 4.00);
 		entity4.addComponent<Visual3DComponent>();
 		Engine::GetInstance().getGraphicsSystem()->getRendererProvider()->getRendererAsyn("assets/shaders/test/test2.vert", "assets/shaders/test/test2.frag",
 			[=](Renderer* render)
@@ -138,7 +139,7 @@ namespace Enternity
 		auto entity5 = createEntity();
 		entity5.getComponent<NameComponent>().name = "sponza";
 		auto& tfc5 = entity5.addComponent<TransformComponent>();
-		tfc5.translation = glm::vec3(-22.85, 5.07, -2.06);
+		tfc5.translation = glm::vec3(-22.85, 5.07, -41.99);
 		tfc5.scale = glm::vec3(0.01, 0.01, 0.01);
 		entity5.addComponent<Visual3DComponent>();
 		Engine::GetInstance().getGraphicsSystem()->getRendererProvider()->getRendererAsyn("assets/shaders/test/test2.vert", "assets/shaders/test/test2.frag",
@@ -158,8 +159,8 @@ namespace Enternity
 		auto entity6 = createEntity();
 		entity6.getComponent<NameComponent>().name = "house";
 		auto& tfc6 = entity6.addComponent<TransformComponent>();
-		tfc6.translation  = glm::vec3(17.36, 3.57, 3.04);
-		tfc6.scale = glm::vec3(0.01, 0.01, 0.01);
+		tfc6.translation  = glm::vec3(51.49, 2.68, -39.41);
+		tfc6.scale = glm::vec3(0.05, 0.05, 0.05);
 		entity6.addComponent<Visual3DComponent>();
 		Engine::GetInstance().getGraphicsSystem()->getRendererProvider()->getRendererAsyn("assets/shaders/test/test2.vert", "assets/shaders/test/test2.frag",
 			[=](Renderer* render)
@@ -178,7 +179,7 @@ namespace Enternity
 		auto entity7 = createEntity();
 		entity7.getComponent<NameComponent>().name = "tree";
 		auto& tfc7 = entity7.addComponent<TransformComponent>();
-		tfc7.translation = glm::vec3(5.26, 2.76, -19.89);
+		tfc7.translation = glm::vec3(5.26, 2.76, -50.18);
 		tfc7.scale = glm::vec3(0.03f);
 		entity7.addComponent<Visual3DComponent>();
 		Engine::GetInstance().getGraphicsSystem()->getRendererProvider()->getRendererAsyn("assets/shaders/test/test2.vert", "assets/shaders/test/tree.frag",
@@ -194,23 +195,62 @@ namespace Enternity
 				comp.mesh = mesh;
 			});
 
-		//test entity 8
 		auto entity8 = createEntity();
-		entity8.getComponent<NameComponent>().name = "particle";
+		entity8.getComponent<NameComponent>().name = "jeep";
 		auto& tfc8 = entity8.addComponent<TransformComponent>();
-		tfc8.translation = glm::vec3(0, 5, 5);
-		entity8.addComponent<ParticleComponent>().renderer;
+		tfc8.translation = glm::vec3(-10.60, 3.57, 3.04);
+		tfc8.scale = glm::vec3(0.01, 0.01, 0.01);
+		entity8.addComponent<Visual3DComponent>();
+		Engine::GetInstance().getGraphicsSystem()->getRendererProvider()->getRendererAsyn("assets/shaders/test/test2.vert", "assets/shaders/test/test2.frag",
+			[=](Renderer* render)
+			{
+				auto& comp = entity8.getComponent<Visual3DComponent>();
+				comp.renderer = render;
+			});
+		Engine::GetInstance().getGraphicsSystem()->getMeshProvider()->getMeshAsyn("assets/models/jeep/jeep.obj",
+			[=](Mesh* mesh)
+			{
+				auto& comp = entity8.getComponent<Visual3DComponent>();
+				comp.mesh = mesh;
+			});
+
+		auto entity9 = createEntity();
+		entity9.getComponent<NameComponent>().name = "hheli";
+		auto& tfc9 = entity9.addComponent<TransformComponent>();
+		tfc9.translation = glm::vec3(-27.68, 0, -19.98);
+		tfc9.scale = glm::vec3(0.10, 0.10, 0.10);
+		entity9.addComponent<Visual3DComponent>();
+		Engine::GetInstance().getGraphicsSystem()->getRendererProvider()->getRendererAsyn("assets/shaders/test/test2.vert", "assets/shaders/test/test2.frag",
+			[=](Renderer* render)
+			{
+				auto& comp = entity9.getComponent<Visual3DComponent>();
+				comp.renderer = render;
+			});
+		Engine::GetInstance().getGraphicsSystem()->getMeshProvider()->getMeshAsyn("assets/models/hheli/hheli.obj",
+			[=](Mesh* mesh)
+			{
+				auto& comp = entity9.getComponent<Visual3DComponent>();
+				comp.mesh = mesh;
+			});
+
+
+		//test particle 
+		auto particle = createEntity();
+		particle.getComponent<NameComponent>().name = "particle";
+		auto& tfcparticle = particle.addComponent<TransformComponent>();
+		tfcparticle.translation = glm::vec3(0, 5, 5);
+		particle.addComponent<ParticleComponent>().renderer;
 		Engine::GetInstance().getGraphicsSystem()->getRendererProvider()->getRendererAsyn("assets/shaders/particle/particle.vert", "assets/shaders/particle/particle.frag",
 			[=](Renderer* render)
 			{
-				auto& comp = entity8.getComponent<ParticleComponent>();
+				auto& comp = particle.getComponent<ParticleComponent>();
 				comp.renderer = render;
 				comp.renderer->setRenderPass({ RenderPass::Fill, true, true });
 			});
 		Engine::GetInstance().getGraphicsSystem()->getMeshProvider()->getMeshAsyn(BasicMeshType::Quad,
 			[=](Mesh* mesh)
 			{
-				auto& comp = entity8.getComponent<ParticleComponent>();
+				auto& comp = particle.getComponent<ParticleComponent>();
 				comp.mesh = mesh;
 			});
 		int width = 256;
@@ -238,7 +278,7 @@ namespace Enternity
 				*((char*)data.getData() + currentPixel + 3) = a;
 			}
 		}
-		entity8.getComponent<ParticleComponent>().texture = 
+		particle.getComponent<ParticleComponent>().texture =
 			Engine::GetInstance().getGraphicsSystem()->getTextureProvider()->getTextureFromData(&data, width, height, channels);
 	}
 
